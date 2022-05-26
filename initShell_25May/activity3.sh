cf270c527f8ef89075e165ad669f048e55d63ab5
EC2 Default User <ec2-user@ip-172-31-27-200.ec2.internal>
2022-05-26 04:40:10 +0000

    progress on splitting delimited user-entered guesses

--- initShell_25May/activity3.sh
+++ initShell_25May/activity3.sh
@@ -9,8 +9,11 @@ read DRAW_COUNT
 
 drawFiles $DRAW_COUNT
 
-echo -e "\nThanks! For each draw, please enter a sequence of 6 numbers, separated by spaces, where the first five are between 1 and 50 (inclusive) and the sixth is between 1 and 10 (inclusive). Each draw entry should be delimited by a semicolon, with your last entry not including the semicolon.\n"
-echo "For example, if you drew twice, a sample entry could look like: '1 2 3 4 5 6;7 8 9 10 11 12 13"
+echo -e "\nThanks! A given draw includes five regular lottery numbers and one bonus number. Now, for each draw, please enter a sequence of 6 numbers, separated by spaces, where the first five are between 1 and 50 (inclusive) and the sixth is between 1 and 10 (inclusive). Each draw entry should be delimited by a semicolon, with your last entry not including the semicolon.\n"
+echo "For example, if you drew twice, a sample entry could look like: '1 2 3 4 5 6;7 8 9 10 11 12'"
 echo -e "\nNow, please enter your lottery numbers: "
 read USER_GUESSES
+
+#echo $USER_GUESSES
+splitGuess $USER_GUESSES
 
